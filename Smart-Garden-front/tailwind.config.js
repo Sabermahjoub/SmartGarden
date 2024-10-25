fa3836170849c8e609048c93cf51276531f9eeba
Saber Mahjoub <saber.mahjoub003@gmail.com>
2024-10-25 11:11:35 +0100

    Dashboard front

--- Smart-Garden-front/tailwind.config.js
+++ Smart-Garden-front/tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
       colors: {
         // Add your custom color here
         lightGreen: '#6E8A61', 
-        darkGreen : '#20350C'
+        darkGreen : '#20350C',
+        customGrey : '#E4E0E1',
+        customWhite : '#F5F7F8',
+        customGreen : '#16423C',
+        darkGrey : '#F6F5F5'
       },
     },
   },
